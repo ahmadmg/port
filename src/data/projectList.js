6982eee5f5663570a9c11110d747394b4ef4da64
Ahmad Alhaji Mustafa <ahmadmg@gmail.com>
2023-03-14 06:57:36 +0100

    Ba

--- src/data/projectList.js
+++ src/data/projectList.js
@@ -1,34 +1,34 @@
-import img1 from "../assets/Stavanger.png";
-import img2 from "../assets/SolarSystem.png";
-import img3 from "../assets/DrumKit.png";
-import img4 from "../assets/Portfolio.png";
+// import img1 from "../assets/Stavanger.png";
+// import img2 from "../assets/SolarSystem.png";
+// import img3 from "../assets/DrumKit.png";
+// import img4 from "../assets/Portfolio.png";
 
 
-export const ProjectList = [
-  {
-    name: "Stavanger",
-    image: img1,
-    skills: ["SiHtml5","SiCss3"],
-    code:"https://github.com/ahmadmg/Parallax-Website-Stavanger",
-    preview:"https://ahmadmg.github.io/Parallax-Website-Stavanger/",
-  }, {
-    name: "Portfolio",
-    image: img4,
-    skills: ["SiHtml5","SiCss3","SiJavascript","SiReact"], 
-    code:"https://github.com/ahmadmg/portfolio",
-    preview:"https://ahmadmg.github.io/portfolio/",
-  },
-   {
-    name: "Drumkit",
-    image: img3,
-    skills: ["SiHtml5","SiCss3","SiJavascript"],
-	 code:"https://github.com/ahmadmg/DrumKit",
-    preview:"https://ahmadmg.github.io/DrumKit/",
-  },{
-    name: "Solar System",
-    image: img2,
-    skills: ["SiHtml5","SiCss3","SiJavascript"], 
-    code:"https://github.com/ahmadmg/GrupperA3SolSystem",
-    preview:"https://ahmadmg.github.io/GrupperA3SolSystem/",
-  },
-]
+// export const ProjectList = [
+//   {
+//     name: "Stavanger",
+//     image: img1,
+//     skills: ["SiHtml5","SiCss3"],
+//     code:"https://github.com/ahmadmg/Parallax-Website-Stavanger",
+//     preview:"https://ahmadmg.github.io/Parallax-Website-Stavanger/",
+//   }, {
+//     name: "Portfolio",
+//     image: img4,
+//     skills: ["SiHtml5","SiCss3","SiJavascript","SiReact"], 
+//     code:"https://github.com/ahmadmg/portfolio",
+//     preview:"https://ahmadmg.github.io/portfolio/",
+//   },
+//    {
+//     name: "Drumkit",
+//     image: img3,
+//     skills: ["SiHtml5","SiCss3","SiJavascript"],
+// 	 code:"https://github.com/ahmadmg/DrumKit",
+//     preview:"https://ahmadmg.github.io/DrumKit/",
+//   },{
+//     name: "Solar System",
+//     image: img2,
+//     skills: ["SiHtml5","SiCss3","SiJavascript"], 
+//     code:"https://github.com/ahmadmg/GrupperA3SolSystem",
+//     preview:"https://ahmadmg.github.io/GrupperA3SolSystem/",
+//   },
+// ]
